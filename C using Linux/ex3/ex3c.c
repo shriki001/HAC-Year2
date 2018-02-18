3c9afe9e7fb428eede08cee9852f1dcd9086c7a0
Michael <36072985+shriki001@users.noreply.github.com>
2018-02-18 11:17:06 +0200

    Update ex3c.c

--- C using Linux/ex3/ex3c.c
+++ C using Linux/ex3/ex3c.c
@@ -10,7 +10,6 @@
  *
  * =============================================================================
  * Written by  	: Kfir Matityahu & Michael Shriki
- * ID	 	   	: 205425960 / 305599417
  * login	   		: kfirma / michaelshr
  * Course/Year   : OS1 2018
  * Year	   		: B
