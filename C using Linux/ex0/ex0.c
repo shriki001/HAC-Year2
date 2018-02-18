4c0f9b6ed5b01900eae40496a41b26222611fc93
Michael <36072985+shriki001@users.noreply.github.com>
2018-02-18 11:11:37 +0200

    Update ex0.c

--- C using Linux/ex0/ex0.c
+++ C using Linux/ex0/ex0.c
@@ -3,7 +3,6 @@
 *          The program read the input file and creates a 2d-dynamic array.
 * =============================================================================
 * Written by  	: Kfir Matityahu & Michael Shriki
-* ID	 	   	: 205425960 / 305599417
 * login	   		: kfirma / michaelshr
 * Course/Year   : OS1 2018
 * Year	   		: B
