395d614b931cacb80a4d246de2ac651c35f3b70c
Michael <36072985+shriki001@users.noreply.github.com>
2018-02-18 11:20:20 +0200

    Update ex5a1.c

--- C using Linux/ex5/ex5a1.c
+++ C using Linux/ex5/ex5a1.c
@@ -13,7 +13,6 @@
 *            3: just remove from the database
 * =============================================================================
 * Written by  	: Kfir Matityahu & Michael Shriki
-* ID	 	   	: 205425960 / 305599417
 * login	   	: kfirma / michaelshr
 * Course/Year  : OS1 2018
 * Year	   		: B
